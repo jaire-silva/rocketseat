0ff8fa49454288a34614c8edccdd9f49e092125b
Jaire Silva <jaire.silva@foxconn.com>
2026-01-08 16:55:11 -0400

    bind de propriedade

--- angular/meu-primeiro-projeto/src/app/app.ts
+++ angular/meu-primeiro-projeto/src/app/app.ts
@@ -2,13 +2,15 @@ import {Component} from '@angular/core';
 import {MeuBotao} from './meu-botao/meu-botao';
 import {BotaoFlat} from './botao-flat/botao-flat';
 import {EventBinding} from './event-binding/event-binding';
+import {PropertyBinding} from './property-binding/property-binding';
 
 @Component({
   selector: 'app-root',
   imports: [
     MeuBotao,
     BotaoFlat,
-    EventBinding
+    EventBinding,
+    PropertyBinding
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
