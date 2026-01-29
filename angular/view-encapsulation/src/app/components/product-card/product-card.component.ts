dbfbde9e016fba5a7adae7a8586bc350a97822e1
Jaire Silva <jaire.silva@foxconn.com>
2026-01-29 16:28:40 -0400

    adicionando componente custom-button e integrando ao product-card

--- angular/view-encapsulation/src/app/components/product-card/product-card.component.ts
+++ angular/view-encapsulation/src/app/components/product-card/product-card.component.ts
@@ -1,8 +1,11 @@
 import {Component, ViewEncapsulation} from '@angular/core';
+import {CustomButtonComponent} from '../custom-button/custom-button.component';
 
 @Component({
   selector: 'app-product-card',
-  imports: [],
+  imports: [
+    CustomButtonComponent
+  ],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.css',
   encapsulation: ViewEncapsulation.Emulated
